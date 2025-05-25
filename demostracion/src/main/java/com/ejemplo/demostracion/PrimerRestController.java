3f2c285455693f80c080a1e62e7bb4f6d43e8304
BrunoVerne <bverne@fi.uba.ar>
2025-05-25 19:49:26 -0300

    Clase 2

--- demostracion/src/main/java/com/ejemplo/demostracion/PrimerRestController.java
+++ demostracion/src/main/java/com/ejemplo/demostracion/PrimerRestController.java
@@ -1,30 +1,129 @@
 package com.ejemplo.demostracion;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import dominio.Persona;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/persona")
 public class PrimerRestController {
 
-    @GetMapping //mapear solicitudes HTTP GET a métodos específicos en un controlador
-    public String saludar(){
-        return "Hola mundo";
+
+
+    private List<Persona> personas;
+
+    public PrimerRestController(List<Persona> personas) {
+        this.personas = new ArrayList<>(
+                Arrays.asList(
+                        new Persona(1L, "lautaro", "martinez", "lau@gmail.com"),
+                        new Persona(2L, "juan", "villalba", "juan@gmail.com"),
+                        new Persona(3L, "pedro", "verne", "pedro@gmail.com")
+                )
+        );
     }
 
-    //get cuando me pasan algún parametro en el endpoint, en el PathVariable específico
-    //le aviso el cambio de nombre de variable si estos son distintos(name != nombre)
-    @GetMapping("/{name}")
-    public String saludarNombre( @PathVariable(name = "name") String nombre){
-        return String.format("Hola %s como estas", nombre);
+    @GetMapping()
+    public List<Persona> getPersonas() {
+        return personas;
     }
 
-    @GetMapping("/{a}/{b}") //aca no aviso el cambio de nombre de variable porque se llaman igual
-    public int sumar(@PathVariable int a, @PathVariable int b){
-        return a  + b;
+    @GetMapping("/filtros")
+    public ResponseEntity<?> buscarPersonaPorNombreApellido(@RequestParam(required = false) String nombre,
+                                                        @RequestParam(required = false) String apellido) {
+
+        if (nombre == null && apellido == null) {
+            return ResponseEntity.badRequest().body("Nombre y apellido vacio");
+
+        }
+
+        List<Persona> response = new ArrayList<>();
+
+        if (nombre != null) {
+            response.addAll(
+                    this.personas.stream()
+                            .filter(persona -> persona.getNombre().equalsIgnoreCase(nombre))
+                            .collect(Collectors.toList())
+            );
+        }
+
+        if (apellido != null) {
+            this.personas.stream()
+                    .filter(persona -> persona.getApellido().equalsIgnoreCase(apellido))
+                    .forEach(persona -> {
+                        if (!response.contains(persona)) {
+                            response.add(persona);
+                        }
+                    });
+        }
+
+        return ResponseEntity.ok(response);
     }
 
 
 
+    @GetMapping("/email/{mail}")
+    public ResponseEntity<?> buscarPersonaPorMail(@PathVariable String mail){
+
+        if (mail == null) {
+            return ResponseEntity.badRequest().body("Nombre y apellido vacio");
+
+        }
+            return this.personas
+                .stream()
+                .filter(persona -> persona.getEmail().equals(mail))
+                .findFirst().
+                orElse(new Persona(null,null,null,null));
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<Persona> crear(@RequestBody Persona persona){
+        persona.setCodigo((long) this.personas.size() + 1);
+        this.personas.add(persona);
+        return ResponseEntity.
+                status(HttpStatus.CREATED)
+                .body(persona);
+    }
+
+
+    @PutMapping("{codigo}")
+    public Persona editar(@PathVariable long codigo,
+            @RequestBody Persona persona){
+
+        if(!esCodigoEsxistente(codigo)){
+            throw new RuntimeException("NO existe persona con ese ID");
+        }
+        return this.personas.
+                stream()
+                .filter(per -> per.getCodigo().equals(codigo))
+                .peek(per -> {
+                    per.setApellido(persona.getApellido());
+                    per.setNombre((persona.getNombre()));
+                    per.setEmail(persona.getEmail());
+                })
+                .findFirst().get();
+
+
+    }
+
+    private boolean esCodigoEsxistente(long codigo){
+        return this.personas.stream().anyMatch(persona -> persona.getCodigo().equals(codigo));
+    }
+
+    @DeleteMapping("/{codigo}")
+    public void eliminar(@PathVariable long codigo){
+        if(!esCodigoEsxistente(codigo)){
+            throw new RuntimeException("NO existe persona con ese ID");
+        }
+
+        this.personas.removeIf(persona -> persona.getCodigo().equals(codigo));
+    }
 
 }
---
+++ demostracion/src/main/java/dominio/Persona.java
@@ -0,0 +1,47 @@
+package dominio;
+
+public class Persona {
+    private Long codigo;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    public Persona(Long codigo, String nombre, String apellido, String email) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
